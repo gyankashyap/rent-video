3a68737a63da2fc58edf9da116e54f88298f7229
gyan kashyap <gyan@getpowerplay.in>
2025-05-31 03:18:30 +0530

    chore: initial commit of Java Spring Boot project with dependencies via Spring Initializr

---
+++ src/test/java/com/project/rent_video/RentVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rent_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
